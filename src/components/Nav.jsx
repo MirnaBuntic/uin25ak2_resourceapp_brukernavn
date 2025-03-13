e377f7cc4bb00d474cd7dc0edab6f929b4b3b5c0
MirnaBuntic <mirnacb@hiof.no>
2025-03-13 22:22:28 +0100

    basically klar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,15 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+
+//Fått hjälp av chatgpt att använda NavLink istället för Link så att den automatiskt får en active classname. Detta så att jag ska kunna lägga sass på bara den som är active.
 
 export default function Nav() {
     return (
         <nav>
-            <Link to="/html">HTML</Link>
-            <Link to="/css">CSS</Link>
-            <Link to="/javascript">JavaScript</Link>
-            <Link to="/react">React</Link>
-            <Link to="/headless-cms">Sanity and headless CMS</Link>
+            <NavLink to="/html">HTML</NavLink>
+            <NavLink to="/css">CSS</NavLink>
+            <NavLink to="/javascript">JavaScript</NavLink>
+            <NavLink to="/react">React</NavLink>
+            <NavLink to="/headless-cms">Sanity and headless CMS</NavLink>
         </nav>
     );
 }
