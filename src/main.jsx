de7f2c55db381ed0be17b5e4da5c8bfa3a3fbea7
MirnaBuntic <mirnacb@hiof.no>
2025-03-13 13:40:54 +0100

    scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/layout.scss'
+import './styles/main.scss'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
