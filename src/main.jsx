08d898476d2691ac7febc30880fb23ee0eb4f525
MirnaBuntic <mirnacb@hiof.no>
2025-05-30 23:25:35 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom"
 import './styles/main.scss'
 import App from './App.jsx'
 
